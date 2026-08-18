45e4e044f561dace951f5a13302fda381a1ce3fa
Amos Mwachari <mwachari705@gmail.com>
2026-08-18 18:06:09 +0300

    Fix missing LOCATIONS import

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { X } from 'lucide-react'
 import { FormField } from './shared'
-import { JOB_TYPES, STATUS_OPTIONS, defaultJobForm } from '../lib/helpers'
+import { JOB_TYPES, LOCATIONS, STATUS_OPTIONS, defaultJobForm } from '../lib/helpers'
 
 export default function JobFormModal({ initialJob, onClose, onSave }) {
   const [form, setForm] = useState(initialJob ? {
